ad1e9748210da9441089b33c4c541f0f1211cd3c
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2019-05-01 10:22:13 +0200

    Makefiles : Sources detection : Now also detects symlinks in src subdirs.

--- mk-inc/project-sources.mk
+++ mk-inc/project-sources.mk
@@ -4,11 +4,11 @@
 # The following macros will basically produce lists of files (sources and objs)
 # from what they found in src/ and tests/ directories.
 
-SOURCES		:= $(shell find $(SRCDIR) -type f -name *.$(SRCEXT))
-SOURCES_TST	:= $(shell find $(TESTSDIR) -type f -name *.$(SRCEXT))
+SOURCES		+= $(shell find $(SRCDIR) \( -type f -o -type l \) -a -name *.$(SRCEXT))
+#SOURCES_TST	:= $(shell find $(TESTSDIR) -type f -name *.$(SRCEXT))
 OBJECTS		:= $(patsubst $(SRCDIR)/%,$(BUILDDIRSRC)/%,$(SOURCES:.$(SRCEXT)=.$(OBJEXT)))
-OBJECTS_TST	:= $(patsubst $(TESTSDIR)/%,$(BUILDDIRTST)/%,$(SOURCES_TST:.$(SRCEXT)=.$(OBJEXT)))
+#OBJECTS_TST	:= $(patsubst $(TESTSDIR)/%,$(BUILDDIRTST)/%,$(SOURCES_TST:.$(SRCEXT)=.$(OBJEXT)))
 
 #Pull in dependency info for *existing* .o files
 -include $(OBJECTS:.$(OBJEXT)=.$(DEPEXT))
--include $(OBJECTS_TST:.$(OBJEXT)=.$(DEPEXT))
+#-include $(OBJECTS_TST:.$(OBJEXT)=.$(DEPEXT))
