3fe7375d595eb9c8945c4dd7bd579f42eb263aab
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2019-05-09 09:51:03 +0200

    ISR : Code update :
    + Removed prints as it could cause bad behaviour, especially when we do a
    print somewhere else in the code while an IT is raised ;
    
    + Renamed const var containing preload value of the timer ;
    
    + Code cleaning in setup_timer1.

--- src/setup.cpp
+++ src/setup.cpp
@@ -27,10 +27,13 @@ void    setup(void)
 
     setup_serial();
 
-    setup_timer1();
 
     /* Initialize the display by creating the FSM singleton instance */
     Display::FSMContext::Instance();
+
+
+    /* This shall be done at last as it enables interrupts */
+    setup_timer1();
 }
 
 /* ########################################################################## */
@@ -63,23 +66,14 @@ void    setup_serial(void)
 void    setup_timer1(void)
 {
     // initialize timer1
-    noInterrupts();           // disable all interrupts
     TCCR1A = 0;
     TCCR1B = 0;
 
-    // Set timer1_counter to the correct value for our interrupt interval
-    //timer1_counter = 64911;   // preload timer 65536-16MHz/256/100Hz
-    //timer1_counter = 64286;   // preload timer 65536-16MHz/256/50Hz
-//    timer1_counterPreload = 34286;   // preload timer 65536-16MHz/256/2Hz
-    g_timer1_counterPreload = 49910;   // preload timer 65536-16MHz/256/2Hz
+    TCNT1   = C_TIMER1_COUNTER_PRELOAD;   // preload timer
+    TCCR1B  |= (1 << CS12);    // 256 prescaler
 
-    TCNT1 = g_timer1_counterPreload;   // preload timer
-//    TCCR1B |= (1 << CS12);    // 256 prescaler
-    // 1024 prescaler
-    TCCR1B |= (1 << CS12);    // 1024 prescaler
-    TCCR1B |= (1 << CS10);    // 1024 prescaler
-    TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
-    interrupts();             // enable all interrupts
+    /* Enable timer 1 overflow interrupt */
+    TIMSK1  |= (1 << TOIE1);   // enable timer overflow interrupt
 }
 
 /* ########################################################################## */
