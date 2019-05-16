1ec3ea7033b2eb49f1a78b2ad6b3b5368f2f88ae
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2019-05-17 00:12:40 +0200

    comm/interface : Implementation interface abstraite.

---
+++ src/comm/interface/AbstractInterface.h
@@ -0,0 +1,45 @@
+#ifndef ABSTRACTINTERFACE_H
+#define ABSTRACTINTERFACE_H
+
+/* Inherited classes */
+
+/* System includes */
+
+/* Libraries includes */
+
+/* Project includes */
+
+
+namespace Comm {
+namespace Interface {
+
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+class   AbstractInterface
+{
+public:
+
+    AbstractInterface(void);
+
+
+
+protected:
+private:
+
+
+
+public:
+protected:
+private:
+
+};
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+} // namespace Interface
+} // namespace Comm
+
+#endif  /*< ABSTRACTINTERFACE_H */
